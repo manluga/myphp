cc99e355f919143209201e79fb5b7b625bbadb58
herve <ch060067@oc6072170846.ibm.com>
2019-09-19 20:24:06 +0200

    test

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ echo "This is a simple web application";
 echo "<br>";
 echo "This is a simple web application";
 echo "This is a simple web application for ex280";
+echo "This is a simple web application for ex280";
 echo "<br>The correct response to the survey is 5 stars";
