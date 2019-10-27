1a9db0d496bb104d640d602bc12ff9283469e7df
Your Name <you@example.com>
2019-10-27 08:57:47 +0100

    aaa

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "aaaaaaaaaa  This is a simple web application";
+echo "This is a simple web application";
 echo "<br>";
 echo "This is a simple web application";
 echo "<br>The correct response to the survey is 5 stars";
